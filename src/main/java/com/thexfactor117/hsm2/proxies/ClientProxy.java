4e41aee0cdc255740df4f85b73c830fa970c63a3
TheXFactor117 <thexfactor117.mods@gmail.com>
2017-07-04 12:22:06 -0400

    Pushing a simple base project

---
+++ src/main/java/com/thexfactor117/hsm2/proxies/ClientProxy.java
@@ -0,0 +1,31 @@
+package com.thexfactor117.hsm2.proxies;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * 
+ * @author TheXFactor117
+ *
+ */
+public class ClientProxy extends CommonProxy
+{
+	@Override
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	@Override
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	@Override
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+}
---
+++ src/main/java/com/thexfactor117/hsm2/proxies/CommonProxy.java
@@ -0,0 +1,28 @@
+package com.thexfactor117.hsm2.proxies;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * 
+ * @author TheXFactor117
+ *
+ */
+public class CommonProxy 
+{
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+}
