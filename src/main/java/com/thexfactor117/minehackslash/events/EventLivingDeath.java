aae4f1d2e5076ca132ac30d14029b90d19af94be
TheXFactor117 <thexfactor117.mods@gmail.com>
2017-07-06 17:49:40 -0400

    Updated player info Gui Includes percent exp to next level Added skill point lang string

--- src/main/java/com/thexfactor117/minehackslash/events/EventLivingDeath.java
+++ src/main/java/com/thexfactor117/minehackslash/events/EventLivingDeath.java
@@ -40,7 +40,11 @@ public class EventLivingDeath
 				
 				// update experience on client AND server; increase level if need be.
 				playerInfo.setPlayerExperience(playerInfo.getPlayerExperience() + experience);
-				if (playerInfo.getPlayerExperience() > playerInfo.getLevelUpExperience(playerInfo.getPlayerLevel())) playerInfo.setPlayerLevel(playerInfo.getPlayerLevel() + 1);
+				if (playerInfo.getPlayerExperience() > playerInfo.getLevelUpExperience(playerInfo.getPlayerLevel())) 
+				{
+					playerInfo.setPlayerLevel(playerInfo.getPlayerLevel() + 1); // increase level
+					playerInfo.setSkillPoints(playerInfo.getSkillPoints() + 1); // increase skill points
+				}
 				MineHackSlash.network.sendTo(new PacketUpdatePlayerInformation(playerInfo), (EntityPlayerMP) player); 
 			}
 		}
