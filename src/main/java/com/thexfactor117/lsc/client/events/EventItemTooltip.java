9a0543ec9841fe44a046c89b4642538555ee4c22
TheXFactor117 <thexfactor117.mods@gmail.com>
2019-05-27 18:10:24 -0400

    new tooltips for everything

--- src/main/java/com/thexfactor117/lsc/client/events/EventItemTooltip.java
+++ src/main/java/com/thexfactor117/lsc/client/events/EventItemTooltip.java
@@ -1,12 +1,10 @@
 package com.thexfactor117.lsc.client.events;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import org.lwjgl.input.Keyboard;
 
 import com.thexfactor117.lsc.capabilities.implementation.LSCPlayerCapability;
-import com.thexfactor117.lsc.config.Configs;
 import com.thexfactor117.lsc.entities.projectiles.Rune;
 import com.thexfactor117.lsc.items.base.ItemBauble;
 import com.thexfactor117.lsc.items.base.weapons.ItemMagical;
@@ -35,7 +33,7 @@ import net.minecraftforge.fml.relauncher.Side;
  *
  */
 @Mod.EventBusSubscriber(value = Side.CLIENT)
-public class EventItemTooltip 
+public class EventItemTooltip
 {
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public static void onItemTooltip(ItemTooltipEvent event)
@@ -43,17 +41,14 @@ public class EventItemTooltip
 		ArrayList<String> tooltip = (ArrayList<String>) event.getToolTip();
 		ItemStack stack = event.getItemStack();
 		NBTTagCompound nbt = NBTHelper.loadStackNBT(stack);
-		
+
 		if (event.getEntityPlayer() != null)
 		{
 			LSCPlayerCapability cap = PlayerUtil.getLSCPlayer(event.getEntityPlayer());
-			
-			if (cap != null && (stack.getItem() instanceof ItemSword || stack.getItem() instanceof ItemArmor || stack.getItem() instanceof ItemBow || 
-					stack.getItem() instanceof ItemMagical || stack.getItem() instanceof ItemBauble))
+
+			if (cap != null && (stack.getItem() instanceof ItemSword || stack.getItem() instanceof ItemArmor || stack.getItem() instanceof ItemBow || stack.getItem() instanceof ItemMagical || stack.getItem() instanceof ItemBauble))
 			{
-				Rarity rarity = Rarity.getRarity(nbt);
-				
-				if (rarity != Rarity.DEFAULT)
+				if (Rarity.getRarity(nbt) != Rarity.DEFAULT)
 				{
 					if (stack.getItem() instanceof ItemSword) drawMelee(tooltip, stack, nbt, event.getEntityPlayer(), cap);
 					else if (stack.getItem() instanceof ItemArmor) drawArmor(tooltip, stack, nbt, event.getEntityPlayer(), cap);
@@ -69,46 +64,168 @@ public class EventItemTooltip
 			}
 		}
 	}
-	
+
 	private static void drawMelee(ArrayList<String> tooltip, ItemStack stack, NBTTagCompound nbt, EntityPlayer player, LSCPlayerCapability cap)
-	{		
+	{
 		if (nbt.hasKey("IsSpecial") && nbt.getBoolean("IsSpecial"))
 		{
 			tooltip.add(1, TextFormatting.LIGHT_PURPLE + "" + TextFormatting.ITALIC + "Special");
 			tooltip.add("");
 		}
-		
+
 		tooltip.add(1, Rarity.getRarity(nbt).getColor() + Rarity.getRarity(nbt).getName() + " " + ItemUtil.getItemOriginalName(stack));
-		
+
 		// Level
 		if (cap.getPlayerLevel() < ItemUtil.getItemLevel(stack)) tooltip.add(TextFormatting.RED + "Level: " + ItemUtil.getItemLevel(stack));
 		else tooltip.add("Level: " + ItemUtil.getItemLevel(stack));
-		
+
 		tooltip.add("");
 		tooltip.add("DPS: " + ItemUtil.FORMAT.format(ItemUtil.getItemDamagePerSecond(stack, cap)));
 		tooltip.add("");
-		
+
 		// Primary Attributes - damage/attack speed
 		tooltip.add(TextFormatting.ITALIC + "Primary Attributes");
 		tooltip.add(TextFormatting.BLUE + " * " + ItemUtil.getItemDamage(stack) + " Damage" + TextFormatting.GRAY + " [" + ItemUtil.getItemMinDamage(stack) + " - " + ItemUtil.getItemMaxDamage(stack) + "]");
 		tooltip.add(TextFormatting.BLUE + " * " + ItemUtil.FORMAT.format((ItemUtil.getItemAttackSpeed(stack) + 4)) + " Attack Speed");
-		
+
 		// Secondary Attributes - attributes
 		if (!ItemUtil.getSecondaryAttributes(stack).isEmpty())
 		{
 			tooltip.add("");
 			tooltip.add(TextFormatting.ITALIC + "Secondary Attributes");
-			
+
 			for (Attribute attribute : ItemUtil.getSecondaryAttributes(stack))
 			{
 				tooltip.add(attribute.getTooltipDisplay(nbt));
 			}
 		}
-		
+
 		// Other Info
+		shiftInfo(tooltip, stack);
+	}
+
+	private static void drawArmor(ArrayList<String> tooltip, ItemStack stack, NBTTagCompound nbt, EntityPlayer player, LSCPlayerCapability cap)
+	{
+		tooltip.add(1, Rarity.getRarity(nbt).getColor() + Rarity.getRarity(nbt).getName() + " " + ItemUtil.getItemOriginalName(stack));
+
+		// Level
+		if (cap.getPlayerLevel() < nbt.getInteger("Level")) tooltip.add(TextFormatting.RED + "Level: " + nbt.getInteger("Level"));
+		else tooltip.add("Level: " + nbt.getInteger("Level"));
+		tooltip.add("");
+
+		// Primary Attributes - armor
+		tooltip.add(TextFormatting.ITALIC + "Primary Attributes");
+		tooltip.add(TextFormatting.BLUE + " * " + ItemUtil.FORMAT.format(ItemUtil.getItemArmor(stack)) + " Armor");
+
+		// Secondary Attributes
+		if (!ItemUtil.getSecondaryAttributes(stack).isEmpty())
+		{
+			tooltip.add("");
+			tooltip.add(TextFormatting.ITALIC + "Secondary Attributes");
+
+			for (Attribute attribute : ItemUtil.getSecondaryAttributes(stack))
+			{
+				tooltip.add(attribute.getTooltipDisplay(nbt));
+			}
+		}
+
+		// Other Info
+		shiftInfo(tooltip, stack);
+	}
+
+	private static void drawRanged(ArrayList<String> tooltip, ItemStack stack, NBTTagCompound nbt, EntityPlayer player, LSCPlayerCapability cap)
+	{
+		tooltip.add(1, Rarity.getRarity(nbt).getColor() + Rarity.getRarity(nbt).getName() + " " + ItemUtil.getItemOriginalName(stack));
+
+		// Level
+		if (cap.getPlayerLevel() < nbt.getInteger("Level")) tooltip.add(TextFormatting.RED + "Level: " + nbt.getInteger("Level"));
+		else tooltip.add("Level: " + nbt.getInteger("Level"));
+		tooltip.add("");
+
+		// Primary Attributes - damage/attack speed
+		tooltip.add(TextFormatting.ITALIC + "Primary Attributes");
+		tooltip.add(TextFormatting.BLUE + " * " + ItemUtil.getItemDamage(stack) + " Damage" + TextFormatting.GRAY + " [" + ItemUtil.getItemMinDamage(stack) + " - " + ItemUtil.getItemMaxDamage(stack) + "]");
+		tooltip.add(TextFormatting.BLUE + " * " + ItemUtil.FORMAT.format((ItemUtil.getItemAttackSpeed(stack) + 4)) + " Attack Speed");
+
+		// Secondary Attributes
+		if (!ItemUtil.getSecondaryAttributes(stack).isEmpty())
+		{
+			tooltip.add("");
+			tooltip.add(TextFormatting.ITALIC + "Secondary Attributes");
+
+			for (Attribute attribute : ItemUtil.getSecondaryAttributes(stack))
+			{
+				tooltip.add(attribute.getTooltipDisplay(nbt));
+			}
+		}
+
+		// Other Info
+		shiftInfo(tooltip, stack);
+	}
+
+	private static void drawMagical(ArrayList<String> tooltip, ItemStack stack, NBTTagCompound nbt, EntityPlayer player, LSCPlayerCapability cap)
+	{
+		tooltip.add(1, Rarity.getRarity(nbt).getColor() + Rarity.getRarity(nbt).getName() + " " + ItemUtil.getItemOriginalName(stack));
+
+		// Level
+		if (cap.getPlayerLevel() < nbt.getInteger("Level")) tooltip.add(TextFormatting.RED + "Level: " + nbt.getInteger("Level"));
+		else tooltip.add("Level: " + nbt.getInteger("Level"));
+		tooltip.add("");
+
+		// Rune
+		tooltip.add(Rune.getRune(nbt).getColor() + Rune.getRune(nbt).getName());
+		tooltip.add("");
+
+		// Primary Attributes - damage/attack speed
+		tooltip.add(TextFormatting.ITALIC + "Primary Attributes");
+		tooltip.add(TextFormatting.BLUE + " * " + ItemUtil.getItemDamage(stack) + " Damage" + TextFormatting.GRAY + " [" + ItemUtil.getItemMinDamage(stack) + " - " + ItemUtil.getItemMaxDamage(stack) + "]");
+		tooltip.add(TextFormatting.BLUE + " * " + ItemUtil.FORMAT.format((ItemUtil.getItemAttackSpeed(stack) + 4)) + " Attack Speed");
+
+		// Secondary Attributes
+		if (!ItemUtil.getSecondaryAttributes(stack).isEmpty())
+		{
+			tooltip.add("");
+			tooltip.add(TextFormatting.ITALIC + "Secondary Attributes");
+
+			for (Attribute attribute : ItemUtil.getSecondaryAttributes(stack))
+			{
+				tooltip.add(attribute.getTooltipDisplay(nbt));
+			}
+		}
+
+		// Other Info
+		shiftInfo(tooltip, stack);
+	}
+
+	private static void drawBauble(ArrayList<String> tooltip, ItemStack stack, NBTTagCompound nbt, EntityPlayer player, LSCPlayerCapability cap)
+	{
+		tooltip.add(1, Rarity.getRarity(nbt).getColor() + Rarity.getRarity(nbt).getName() + " " + ItemUtil.getItemOriginalName(stack));
+
+		// Level
+		if (cap.getPlayerLevel() < nbt.getInteger("Level")) tooltip.add(TextFormatting.RED + "Level: " + nbt.getInteger("Level"));
+		else tooltip.add("Level: " + nbt.getInteger("Level"));
+
+		// Secondary Attributes
+		if (!ItemUtil.getSecondaryAttributes(stack).isEmpty())
+		{
+			tooltip.add("");
+			tooltip.add(TextFormatting.ITALIC + "Secondary Attributes");
+
+			for (Attribute attribute : ItemUtil.getSecondaryAttributes(stack))
+			{
+				tooltip.add(attribute.getTooltipDisplay(nbt));
+			}
+		}
+
+		// Other Info
+		shiftInfo(tooltip, stack);
+	}
+
+	private static void shiftInfo(ArrayList<String> tooltip, ItemStack stack)
+	{
 		tooltip.add("");
 		tooltip.add(TextFormatting.ITALIC + "Shift for additional info");
-		
+
 		if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT))
 		{
 			tooltip.add(TextFormatting.DARK_GRAY + "Required Level: ");
@@ -117,130 +234,5 @@ public class EventItemTooltip
 			tooltip.add("");
 			tooltip.add(TextFormatting.DARK_GRAY + "Attribute's text color represents the attribute's rarity.");
 		}
-	}
-	
-	private static void drawArmor(ArrayList<String> tooltip, ItemStack stack, NBTTagCompound nbt, EntityPlayer player, LSCPlayerCapability cap)
-	{
-		tooltip.add(1, Rarity.getRarity(nbt).getColor() + Rarity.getRarity(nbt).getName() + " " + ItemUtil.getItemOriginalName(stack));
-		
-		// Level
-		if (cap.getPlayerLevel() < nbt.getInteger("Level")) tooltip.add(TextFormatting.RED + "Level: " + nbt.getInteger("Level"));
-		else tooltip.add("Level: " + nbt.getInteger("Level"));
-		tooltip.add("");
-		
-		// Primary Attributes - armor
-		tooltip.add(TextFormatting.ITALIC + "Primary Attributes");
-		tooltip.add(TextFormatting.BLUE + " * " + ItemUtil.FORMAT.format(ItemUtil.getItemArmor(stack)) + " Armor");
-		
-		// Secondary Attributes
-		if (!ItemUtil.getSecondaryAttributes(stack).isEmpty())
-		{
-			tooltip.add("");
-			tooltip.add(TextFormatting.ITALIC + "Secondary Attributes");
-			
-			for (Attribute attribute : ItemUtil.getSecondaryAttributes(stack))
-			{
-				tooltip.add(attribute.getTooltipDisplay(nbt));
-			}
-		}
-	}
-	
-	private static void drawRanged(ArrayList<String> tooltip, ItemStack stack, NBTTagCompound nbt, EntityPlayer player, LSCPlayerCapability cap)
-	{
-		tooltip.add(1, "");
-		tooltip.add(Rarity.getRarity(nbt).getColor() + Rarity.getRarity(nbt).getName());
-		
-		// Level
-		if (cap.getPlayerLevel() < nbt.getInteger("Level")) tooltip.add(TextFormatting.RED + "Level: " + nbt.getInteger("Level"));
-		else tooltip.add("Level: " + nbt.getInteger("Level"));
-		
-		tooltip.add("");
-		
-		// Damage and Attack Speed
-		DecimalFormat format = new DecimalFormat("#.##");
-		double attackSpeed = nbt.getDouble("AttackSpeed") + (Configs.playerCategory.attackSpeedMultiplier * (cap.getTotalAgility()));
-
-		tooltip.add(TextFormatting.BLUE + "+" + nbt.getInteger("MinDamage") + "-" + nbt.getInteger("MaxDamage") + " Damage");
-		tooltip.add(TextFormatting.BLUE + "+" + format.format(attackSpeed) + " Attack Speed");
-		tooltip.add("");
-		
-		// Durability
-		tooltip.add("Durability: " + (stack.getMaxDamage() - stack.getItemDamage()) + " / " + stack.getMaxDamage());
-		tooltip.add("");
-		
-		// Attributes
-		tooltip.add(TextFormatting.ITALIC + "Attributes");
-		
-	}
-	
-	private static void drawMagical(ArrayList<String> tooltip, ItemStack stack, NBTTagCompound nbt, EntityPlayer player, LSCPlayerCapability cap)
-	{
-		/*
-		 * NAME
-		 * Level
-		 * 
-		 * Rune
-		 * 
-		 * Damage
-		 * Attack Speed
-		 * 
-		 * Durability
-		 * 
-		 * Attributes
-		 */
-		
-		tooltip.add(1, "");
-		tooltip.add(Rarity.getRarity(nbt).getColor() + Rarity.getRarity(nbt).getName());
-		
-		// Level
-		if (cap.getPlayerLevel() < nbt.getInteger("Level")) tooltip.add(TextFormatting.RED + "Level: " + nbt.getInteger("Level"));
-		else tooltip.add("Level: " + nbt.getInteger("Level"));
-		
-		tooltip.add("");
-		
-		// Rune
-		tooltip.add(Rune.getRune(nbt).getColor() + Rune.getRune(nbt).getName());
-		tooltip.add("");
-		
-		// Damage and Attack Speed
-		DecimalFormat format = new DecimalFormat("#.##");
-		double attackSpeed = nbt.getDouble("AttackSpeed") + (Configs.playerCategory.attackSpeedMultiplier * (cap.getTotalAgility()));
-
-		tooltip.add(TextFormatting.BLUE + "+" + nbt.getInteger("MinDamage") + "-" + nbt.getInteger("MaxDamage") + " Damage");
-		tooltip.add(TextFormatting.BLUE + "+" + format.format(attackSpeed) + " Attack Speed");
-		tooltip.add("");
-		
-		// Durability
-		tooltip.add("Durability: " + (stack.getMaxDamage() - stack.getItemDamage()) + " / " + stack.getMaxDamage());
-		tooltip.add("");
-		
-		// Attributes
-		tooltip.add(TextFormatting.ITALIC + "Attributes");
-		
-	}
-	
-	private static void drawBauble(ArrayList<String> tooltip, ItemStack stack, NBTTagCompound nbt, EntityPlayer player, LSCPlayerCapability cap)
-	{
-		/*
-		 * NAME
-		 * Level
-		 * 
-		 * Attributes
-		 */
-		
-		tooltip.add(1, "");
-		tooltip.add(Rarity.getRarity(nbt).getColor() + Rarity.getRarity(nbt).getName());
-		
-		// Level
-		if (cap.getPlayerLevel() < nbt.getInteger("Level")) tooltip.add(TextFormatting.RED + "Level: " + nbt.getInteger("Level"));
-		else tooltip.add("Level: " + nbt.getInteger("Level"));
-		
-		tooltip.add("");
-		
-		// Attributes
-		tooltip.add(TextFormatting.ITALIC + "Attributes");
-		
-		
-		tooltip.add("");
 	}
 }
